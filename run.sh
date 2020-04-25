37675a3ff9f375ac8f8af923c310c1ca354e39e7
Ayan Biswas <ayanbiswas184@gmail.com>
2020-04-25 19:30:03 +0530

    file structure changed

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python people_counter.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt --model mobilenet_ssd/MobileNetSSD_deploy.caffemodel --output output.avi
+python 'main/people_counter.py' --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt --model mobilenet_ssd/MobileNetSSD_deploy.caffemodel --output output.avi
